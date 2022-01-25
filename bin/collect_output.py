1c463ec134e9f284b29feec25d3e8decbfb1c50c
Vasyl Vaskivskyi <vaskivskyi.v@gmail.com>
2022-01-25 14:01:54 +0000

    output is no longer separated into region dirs

--- bin/collect_output.py
+++ bin/collect_output.py
@@ -11,27 +11,21 @@ def collect_segm_masks(data_dir: Path, listing: dict, out_dir: Path):
     img_name_template = "reg{region:03d}_mask.ome.tiff"
     dir_name_template = "region_{region:03d}"
     for region, channels in listing.items():
-        out_reg_dir = out_dir / dir_name_template.format(region=region)
-        make_dir_if_not_exists(out_reg_dir)
         src = (
             data_dir
             / dir_name_template.format(region=region)
             / img_name_template.format(region=region)
         )
-        dst = out_reg_dir / img_name_template.format(region=region)
+        dst = out_dir / img_name_template.format(region=region)
         shutil.copy(src, dst)
         print("region:", region, "| src:", src, "| dst:", dst)
 
 
 def collect_expr(data_dir: Path, listing: Dict[int, Dict[str, str]], out_dir: Path):
     out_name_template = "reg{region:03d}_expr.ome.tiff"
-    out_dir_name_template = "region_{region:03d}"
-
     for region in listing:
         channel_map = listing[region]
-        out_reg_dir = out_dir / out_dir_name_template.format(region=region)
-        make_dir_if_not_exists(out_reg_dir)
-        out_path = out_reg_dir / out_name_template.format(region=region)
+        out_path = out_dir / out_name_template.format(region=region)
         bundle_imgs_in_region(data_dir, channel_map, out_path)
 
 
